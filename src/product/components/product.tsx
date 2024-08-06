709e411d00d20b6bfd699289ff06092601c42d78
anoopss <anoopviswanadh007@gmail.com>
2024-08-06 23:18:55 +0530

    feat: file seperated

---
+++ src/product/components/product.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { MoreHorizontal } from 'lucide-react';
+import { TableCell, TableRow } from '@/components/ui/table';
+
+export function Product({ product }: { product: any }) {
+  return (
+    <TableRow>
+      <TableCell className="hidden sm:table-cell">
+        <Image
+          alt="Product image"
+          className="aspect-square rounded-md object-cover cursor-pointer"
+          height="48"
+          src={product.imageUrl}
+          width="48"
+        />
+      </TableCell>
+      <TableCell className="font-medium cursor-pointer">
+        {product.name}
+      </TableCell>
+      <TableCell>
+        <Badge variant="outline" className="capitalize">
+          {product.status}
+        </Badge>
+      </TableCell>
+      <TableCell className="hidden md:table-cell">{`$${product.price}`}</TableCell>
+      <TableCell className="hidden md:table-cell">{product.stock}</TableCell>
+      <TableCell className="hidden md:table-cell">
+        {product.availableAt.toLocaleDateString()}
+      </TableCell>
+      <TableCell>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button aria-haspopup="true" size="icon" variant="ghost">
+              <MoreHorizontal className="h-4 w-4" />
+              <span className="sr-only">Toggle menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>
+              <form action={() => {}}>
+                <button type="submit">Delete</button>
+              </form>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </TableCell>
+    </TableRow>
+  );
+}
---
+++ src/product/index.tsx
@@ -0,0 +1,3 @@
+import ProductList from './view/productList';
+
+export default { ProductList };
